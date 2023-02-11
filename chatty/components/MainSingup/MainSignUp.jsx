5642fbb21ac00ee4da4a0e94ea51dcd1d48d517e
ShahAli <senayousofiali@gmail.com>
2023-02-11 07:37:46 -0800

    new signup

--- chatty/components/MainSingup/MainSignUp.jsx
+++ chatty/components/MainSingup/MainSignUp.jsx
@@ -178,60 +178,67 @@ export default function MainSignUp() {
 
     return (
         <>  
-            <form className="flex md:flex-row flex-col flex-wrap justify-center items-center
-            bg-gray-50
-            h-full w-[92%] mt-3 rounded-md mx-auto gap-x-14 p-4
-            transition-all duration-300 " >
-                
-                <div className="md:gap-y-10 gap-y-7 flex flex-col w-full md:w-[40%] items-start justify-start
-                md:px-0">
-                    
-                    
-                    <div className="w-full ">
-                        <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 ">Username </label>
-                        <input onChange={(e) => setUsername(e.target.value)} type="text" name="username" id="username" placeholder="username e g jhon" className="bg-gray-50 border-none outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"  />
+            <div className="h-screen md:flex">
+                <div
+                    className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+                    <div>
+                        <h1 className="text-white font-bold text-4xl font-sans">GoFinance</h1>
+                        <p className="text-white mt-1">The most popular peer to peer lending at SEA</p>
+                        <button type="submit" className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button>
                     </div>
-                    
-                    <div className="w-full">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password </label>
-                        <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border-none outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"  />
-                    </div>
-                    <div className="w-full">
-                        <label htmlFor="confirm" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
-                        <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" name="confirm" id="confirm" placeholder="••••••••" className="bg-gray-50 border-none outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "  />
-                    </div>
+                    <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+                    <div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+                    <div className="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+                    <div className="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
                 </div>
-                
-                <div className="mt-4 md:w-[30%] w-full items-start justify-start">
-                    
-                    <div className="w-full">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Bio (optional)</label>
-                        <textarea id="message" onChange={(e) => setBio(e.target.value)} rows="4" className="border-none outline-none p-2.5 w-full text-sm text-gray-900 rounded-md" placeholder="Write you bio ..." ></textarea>
-                    </div>
-                    
-                </div>
-                <div  className="flex items-center justify-center w-[70%] mx-auto mt-4 group">
-                    <label  htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 bg-[#f0f1ed]  rounded-lg cursor-pointer">
-                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                            <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400 transition-all duration-300 
-                            group-hover:-translate-y-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                            <p className="text-sm text-gray-400">File name: {imageDetails?.name || "NA"} </p>
-                            <p className="text-sm text-gray-400">File Size: {imageDetails?.size / 1000000 || "NA"} MB(s)  </p>
-                            <p className="text-sm text-gray-400">File Type: {imageDetails?.type?.split("/")[1]  || "NA"} </p>
-                            
-                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF, JPEG are allowed</p>
+                <div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
+                    <form className="bg-white">
+                        <h1 className="text-gray-800 font-bold text-2xl mb-1">Make An Account</h1>
+                        <p> </p>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Full name" />
                         </div>
-                        <input onChange={checkImageDetailsBeforSubmit}  ref={imageFileRef} id="dropzone-file" type="file" className="hidden" />
-                    </label>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Username" />
+                        </div>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+                        </div>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fill-rule="evenodd"
+                                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" />
+                        </div>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mt-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 text-gray-400">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            <input className=" w-full text-sm text-gray-400  ml-2" id="file_input" type="file" placeholder='Select Profile Image'/>
+                        </div>
+                        <button type="submit" className="block w-full bg-indigo-400 transition-all duration-300 hover:bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
+                        <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
+                    </form>
                 </div>
-                <div className="w-full text-center mt-5 flex items-center justify-center">
-                    <button onClick={handleSubmit}  type="button" className="w-fit flex items-center justify-center text-white bg-[#29B6F6] font-medium rounded-lg text-sm px-5 py-2.5 text-center  gap-x-2 group">
-                        Create Account
-                    <svg className="transition-all duration-150 group-hover:-translate-y-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>
-                    </button> 
-                </div>
-            </form>
+            </div>
        </>
     )
 }
