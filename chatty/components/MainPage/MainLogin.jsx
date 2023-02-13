32786973dc2dc98b80c6a5cc2e8b247ac8adf51e
ShahAli <senayousofiali@gmail.com>
2023-02-12 21:15:19 -0800

    hello

--- chatty/components/MainPage/MainLogin.jsx
+++ chatty/components/MainPage/MainLogin.jsx
@@ -5,10 +5,11 @@ export default function MainLogin () {
         <>
         <div className="flex items-center min-h-screen bg-gray-50 shadow-sm shadow-black">
             <div className="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
-                <div className="flex flex-col md:flex-row">
-                    <div className="h-32 md:h-auto md:w-1/2">
-                        <img className="object-cover w-full h-full" src="https://source.unsplash.com/user/erondu/1600x900"
-                            alt="img" />
+                <div className="flex flex-col md:flex-row rounded-md">
+                    <div className="h-full w-full md:h-auto md:w-1/2 bg-indigo-500 flex items-center justify-center flex-col">
+                        <h1 className="text-white font-bold text-4xl font-sans">LetsChat</h1>
+                        <p className="text-white mt-1">The most popular Chat Web Application</p>
+                        <button type="submit" className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button>
                     </div>
                     <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2 gap-y-10">
                         <div className="w-full gap-y-10">
@@ -22,20 +23,19 @@ export default function MainLogin () {
                                 Login to Your Account
                             </h1>
                             <div>
-                                <label className="block text-sm mb-2">
-                                    Email
-                                </label>
                                 <input type="email"
-                                    className="w-full px-4 py-2 text-sm  rounded-md border border-black"
+                                    className="w-full px-4 py-2 text-sm border-none outline-none bg-gray-100"
                                     placeholder="Email" />
                             </div>
-                            <div className="">
-                                <label className="block mt-4 text-sm mb-2">
-                                    Password
-                                </label>
+                            <div className="mt-4 flex items-center">
                                 <input
-                                    className="w-full px-4 py-2 text-sm  rounded-md border border-black"
-                                    placeholder="Password" type="password" />
+                                    className="w-full px-4 py-2 border-none outline-none bg-gray-100"
+                                    placeholder="Password" 
+                                    type="password" 
+                                />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                </svg>
                             </div>
                             <p class="mt-4">
                                 <Link className="text-sm text-blue-600 hover:underline" href="./forgot-password.html">
@@ -52,7 +52,6 @@ export default function MainLogin () {
                                 href="#">
                                 Log in
                             </button>
-                            <hr className="my-8" />
                         </div>
                     </div>
                 </div>
