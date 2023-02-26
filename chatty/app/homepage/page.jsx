a6535d90f1860b51c61eb070cc880a85d1ba3471
ShahAli <senayousofiali@gmail.com>
2023-02-26 17:58:52 +0430

    rooms comp

--- chatty/app/homepage/page.jsx
+++ chatty/app/homepage/page.jsx
@@ -1,11 +1,15 @@
 import MainLogin from "@/components/MainPage/MainLogin";
 import Navbar from "@/components/Navbar/Navbar";
+import Rooms from "@/components/Rooms/Rooms";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
 export default function page() {
   return (
    <>
    <Navbar />
+   <div className="w-full h-full">
+      <Rooms />
+   </div>
    </>
   )
 }
