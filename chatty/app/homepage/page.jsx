77824cc55b414737b19c3efaf9a2828b42ae2af1
ShahAli <senayousofiali@gmail.com>
2023-02-27 09:26:35 +0430

    back-end starter

--- chatty/app/homepage/page.jsx
+++ chatty/app/homepage/page.jsx
@@ -1,5 +1,6 @@
 import MainLogin from "@/components/MainPage/MainLogin";
 import Navbar from "@/components/Navbar/Navbar";
+import OnlineMembers from "@/components/OnlineMembers/OnlineMembers";
 import Rooms from "@/components/Rooms/Rooms";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
@@ -7,8 +8,11 @@ export default function page() {
   return (
    <>
    <Navbar />
-   <div className="w-full h-full">
-      <Rooms />
+   <div className="w-full h-full flex">
+      <div className="flex flex-col ml-4 mt-2">
+        <Rooms />
+        <OnlineMembers />
+      </div>
    </div>
    </>
   )
