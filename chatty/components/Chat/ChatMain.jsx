c86872b56464d9311dda5b2e5c48a960c108fc02
ShahAli <senayousofiali@gmail.com>
2023-02-28 09:47:45 +0430

    back-end epxress

---
+++ chatty/components/Chat/ChatMain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChatMain() {
+    return (
+        <div className="w-full h-full overflow-scroll border-1 border-gray-300">
+            Main Chat Page
+        </div>
+    );
+}
