b6e3f84a22355682afbe013b3094bfe7ed8e3c5d
ShahAli <senayousofiali@gmail.com>
2023-03-07 08:24:09 +0430

    backend starter

--- back-end/index.js
+++ back-end/index.js
@@ -7,3 +7,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
+const server = require("http").createServer(app);
+const PORT = 5000;
+const socket = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+server.listen(PORT, () => console.log("Started"));
