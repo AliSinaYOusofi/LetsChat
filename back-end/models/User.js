fb889a07eb318e4b496494fa8aa29620e579e29d
ShahAli <senayousofiali@gmail.com>
2023-03-08 09:05:10 +0430

    routes

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,2 +1,38 @@
 const mongoose = require("mongoose");
-require("dotenv").config("");
+const {isEmail} = require("validator");
+const bcrypt = require("bcrypt");
+
+
+const UserModel = new mongoose.Schema({
+    full_name: {
+        type: String,
+        required: [true, "Can't be empty"]
+    },
+
+    username: {
+        type: String,
+        required: [true, "Can't be empty"]
+    },
+
+    email: {
+        type: String,
+        required: [true, "Can't be empty"],
+        lowercase: true,
+        unique: true,
+        index: true,
+        validate: [isEmail, "is not an email"]
+    },
+
+    password: {
+        type: String,
+        required: [true, "Can't be empty"]
+    },
+
+    status: {
+        type: String,
+        default: "online"
+    }
+});
+
+const User = mongoose.model("User", UserModel);
+module.exports = User;
---
+++ back-end/routes/UserRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// creating an account on chatty
+router.post("/signup", async (req, res) => {
+    
+    try {
+        const {full_name, username, email, password} = req.body;
+        const user = await User.create({
+            full_name,
+            username,
+            email,
+            password: await bcrypt.hash(password, bcrypt.genSaltSync(10))
+        });
+        res.status(201).json({message: "created"});
+    } catch (error) {
+        let msg
+        if (error.code == 11000) msg = "user already reged";
+        else msg = error.message;
+        res.status(400).json(msg);
+    }
+});
